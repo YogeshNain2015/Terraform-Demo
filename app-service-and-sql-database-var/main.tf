d3b3b6c2d8877dd859d32a774f49ed75be2c4777
YogeshNain2015 <32641131+YogeshNain2015@users.noreply.github.com>
2021-05-31 21:22:36 +0530

    Update main.tf

--- app-service-and-sql-database-var/main.tf
+++ app-service-and-sql-database-var/main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
+
+
+
+
 resource "azurerm_resource_group" "RG-Terraform" {
   name     = "terraform-resource-group"
   location = "West Europe"
